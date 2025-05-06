d64c589bc5f9836645513fad33f414bb856c1fb9
WSYANGSX <1062033103@qq.com>
2025-05-06 16:07:52 +0800

    modified

--- src/machine_learning/models/darknet.py
+++ src/machine_learning/models/darknet.py
@@ -68,7 +68,7 @@ class Darknet(BaseNet):
             ]
         )
 
-    def forward(self, x: torch.Tensor) -> list[torch.Tensor]:
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor]:
         skips = []
 
         for i, layer in enumerate(self.layers):
@@ -76,7 +76,11 @@ class Darknet(BaseNet):
             if i in [6, 15, 24]:
                 skips.append(x)
 
-        return skips
+        deep_feature = skips.pop()
+        mid_feature = skips.pop()
+        shallow_feature = skips.pop()
+
+        return shallow_feature, mid_feature, deep_feature
 
     def view_structure(self) -> None:
         from torchinfo import summary
@@ -95,7 +99,7 @@ class FPN(BaseNet):
 
         self.num_anchors = num_anchors
         self.num_classes = num_classes
-        self.channels = (5 + self.num_classes) * self.num_classes
+        self.channels = (5 + self.num_classes) * self.num_anchors
 
         # 特征金字塔网络（FPN）
         self.fpn = nn.ModuleDict(
@@ -113,7 +117,9 @@ class FPN(BaseNet):
             }
         )
 
-    def forward(self, skips: list[torch.Tensor]) -> torch.Tensor:
+    def forward(
+        self, shallow_feature: torch.Tensor, mid_feature: torch.Tensor, deep_feature: torch.Tensor
+    ) -> tuple[torch.Tensor]:
         """特征金字塔网络前向传播
 
         Args:
@@ -126,17 +132,17 @@ class FPN(BaseNet):
         # skips: [(256,52,52),(512,26,26),(1024,13,13)]
 
         # ----- 第一层检测 -----
-        x3 = self.fpn.conv_x3(skips.pop())  # (1024,13,13) -> (512,13,13)
+        x3 = self.fpn.conv_x3(deep_feature)  # (1024,13,13) -> (512,13,13)
         detection3 = self.fpn.head_conv3(x3)  # (512,13,13) -> (255,13,13)
         x3_up = self.fpn.upsample(x3)  # (512,13,13) -> (512,26,26)
 
         # ----- 第二层检测 -----
-        x2 = torch.cat([x3_up, self.fpn.conv_x2(skips.pop())], dim=1)  # (512,26,26) cat (256,26,26) -> (768,26,26)
+        x2 = torch.cat([x3_up, self.fpn.conv_x2(mid_feature)], dim=1)  # (512,26,26) cat (256,26,26) -> (768,26,26)
         detection2 = self.fpn.head_conv2(x2)  # (768,26,26) -> (255,26,26)
         x2_up = self.fpn.upsample(x2)  # (768,26,26) -> (768,52,52)
 
         # ----- 第三层检测 -----
-        x1 = torch.cat([x2_up, self.fpn.conv_x1(skips.pop())], dim=1)  # (768,52,52) cat (128,52,52) -> (896,52,52)
+        x1 = torch.cat([x2_up, self.fpn.conv_x1(shallow_feature)], dim=1)  # (768,52,52) cat (128,52,52) -> (896,52,52)
         detection1 = self.fpn.head_conv1(x1)  # (896,52,52) -> (255,13,13)
 
         return detection1, detection2, detection3  # 对应52x52, 26x26, 13x13三个尺度
@@ -151,15 +157,17 @@ class FPN(BaseNet):
             torch.randn(1, 1024, 13, 13),  # 深层特征图
         ]
 
-        # 定义适配函数（正确写法）
-        def forward_wrapper(model, x):
-            return model(x)  # 直接返回模型输出
-
         # 正确调用方式
-        summary(
-            self,
-            input_data=dummy_input,
-            depth=3,
-            col_names=["input_size", "output_size", "num_params", "kernel_size"],
-            verbose=0,
-        )
+        summary(self, input_data=dummy_input)
+
+
+if __name__ == "__main__":
+    fpn = FPN(3, 80)
+    fpn.view_structure()
+    dummy_input = [
+        torch.randn(1, 256, 52, 52),  # 对应浅层特征图
+        torch.randn(1, 512, 26, 26),  # 中层特征图
+        torch.randn(1, 1024, 13, 13),  # 深层特征图
+    ]
+    det1, det2, det3 = fpn(*dummy_input)
+    print(det1.shape, det2.shape, det3.shape)
