0823fb5381a9c6689258604072aa76f5edafbbca
WSYANGSX <1062033103@qq.com>
2025-06-06 09:31:47 +0800

    modified

--- tests/models.py
+++ tests/models.py
@@ -1,13 +1,9 @@
-import torch
 import numpy as np
+import torch
 
-a = torch.tensor(np.array([])).reshape(-1, 3)
-b = torch.tensor(np.array([[1, 2, 3]]))
-c = torch.tensor(np.array([[4, 5, 6], [7, 8, 9]]))
 
-d, e = list(zip((2, b), (3, c)))
-print(d, e)
-for i, boxes in enumerate(e):
-    boxes[:, 0] = i
-bboxes = torch.cat([a, b, c], 0)
-print(bboxes)
+a = np.empty(0).reshape(-1, 5)
+print(a.shape)
+print(a[0])
+b = np.array([]).reshape(-1, 5)
+print(b.shape)
