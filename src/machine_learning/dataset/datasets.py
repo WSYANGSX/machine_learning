4305b04b729493751c40f8357d16b3150df96b0b
WSYANGSX <1062033103@qq.com>
2025-09-12 10:16:45 +0800

    modified

--- src/machine_learning/dataset/datasets.py
+++ src/machine_learning/dataset/datasets.py
@@ -1,40 +1,34 @@
-from typing import Any, Union
+from typing import Any, Optional, Union, Literal
 
-import os
 import cv2
 import math
-import random
-import psutil
 import numpy as np
-
-from tqdm import tqdm
 from pathlib import Path
 from copy import deepcopy
-from torch.utils.data import Dataset
-from multiprocessing.pool import ThreadPool
 
 from machine_learning.utils.logger import LOGGER
 from machine_learning.types.aliases import FilePath
-from machine_learning.utils.constants import NUM_THREADS, IMG_TYPES
-from ultralytics.data.augment import (
-    Compose,
-    Format,
-    Instances,
-    LetterBox,
-    RandomLoadText,
-    classify_augmentations,
-    classify_transforms,
-    v8_transforms,
-)
+from machine_learning.dataset.base import DatasetBase
 
 
 class YoloDataset(DatasetBase):
     """
-    Dataset class for loading object detection and/or segmentation labels in YOLO format.
+    YoloDataset class for loading object detection and/or segmentation labels in YOLO format.
 
     Args:
-        data (dict, optional): A dataset YAML dictionary. Defaults to None.
-        task (str): An explicit arg to point current task, Defaults to 'detect'.
+        imgs (list[str] | np.ndarray): Path list to imgs or imgs itself with np.ndarray format.
+        labels (list[str] | np.ndarray): Path list to the labels or labels itself with np.ndarray format.
+        imgsz (list[str] | np.ndarray): Image size. Defaults to 640.
+        rect (bool, optional): If True, rectangular training is used. Defaults to False.
+        stride (int, optional): Stride. Defaults to 32.
+        pad (float, optional): Padding. Defaults to 0.0.
+        single_cls (bool, optional): If True, single class training is used. Defaults to False.
+        classes (list): List of included classes. Default is None.
+        cache (bool, optional): Cache data to RAM or disk during training. Defaults to False.
+        augment (bool, optional): If True, data augmentation is applied. Defaults to True.
+        hyp (dict, optional): Hyperparameters to apply data augmentation. Defaults to None.
+        batch_size (int, optional): Size of batches. Defaults to None.
+        fraction (float): Fraction of dataset to utilize. Default is 1.0 (use all data).
 
     Returns:
         (torch.utils.data.Dataset): A PyTorch dataset object that can be used for training an object detection model.
@@ -42,8 +36,15 @@ class YoloDataset(DatasetBase):
 
     def __init__(
         self,
-        imgs: np.ndarray | list[str],
-        labels: np.ndarray | list[str],
+        imgs: np.ndarray | list[FilePath],
+        labels: np.ndarray | list[FilePath],
+        imgsz: int = 640,
+        task: Literal["detect", "pose", "segment"] = "detect",
+        rect: bool = False,
+        stride: int = 32,
+        pad: float = 0.5,
+        single_cls: bool = False,
+        classes: list[int] = None,
         cache: bool = False,
         augment: bool = True,
         hyp: dict[str, Any] | None = None,
@@ -51,117 +52,132 @@ class YoloDataset(DatasetBase):
         fraction: float = 1.0,
     ):
         """Initialize BaseDataset with given configuration and options."""
-        super().__init__()
+        self.imgsz = imgsz
+        self.rect = rect
+        self.stride = stride
+        self.pad = pad
+        self.single_cls = single_cls
+        self.classes = classes
+        self.task = task
+        self.use_segments = self.task == "segment"
+        self.use_keypoints = self.task == "pose"
+        self.use_obb = self.task == "obb"
 
-        self.data = []
-        self.labels = []
+        super().__init__(
+            data=imgs,
+            labels=labels,
+            cache=cache,
+            augment=augment,
+            hyp=hyp,
+            batch_size=batch_size,
+            fraction=fraction,
+        )  # cache imgs and labels
 
-        self.augment = augment
-        self.fraction = fraction
-        self.batch_size = batch_size
+        if self.rect:
+            assert self.batch_size is not None
+            self.set_rectangle()
 
-        # Buffer thread for data fusion
-        self.buffer = []
-        self.max_buffer_length = min((self.length, self.batch_size * 8, 1000)) if self.augment else 0
+    def set_rectangle(self):
+        """Sets the shape of bounding boxes for YOLO detections as rectangles."""
+        bi = np.floor(np.arange(self.length) / self.batch_size).astype(int)  # batch index
+        nb = bi[-1] + 1  # number of batches
 
-        # Transforms
-        self.transforms = self.build_transforms(hyp=hyp)
+        s = np.array([x.pop("shape") for x in self.labels])  # hw
+        ar = s[:, 0] / s[:, 1]  # aspect ratio
+        irect = ar.argsort()
+        self.im_files = [self.im_files[i] for i in irect]
+        self.labels = [self.labels[i] for i in irect]
+        ar = ar[irect]
 
-    def preprocess_data(self, imgs: np.ndarray | list[str], labels: np.ndarray | list[str]):
-        # Cache data (options are cache = True, False, None, "ram", "disk")
-        if isinstance(data, np.ndarray):  # full load mode, ignore cache param
-            self.data = data[: round(len(labels) * self.fraction)]
-            self.labels = labels[: round(len(labels) * self.fraction)]
-            self.length = len(self.labels)
+        # Set training image shapes
+        shapes = [[1, 1]] * nb
+        for i in range(nb):
+            ari = ar[bi == i]
+            mini, maxi = ari.min(), ari.max()
+            if maxi < 1:
+                shapes[i] = [maxi, 1]
+            elif mini > 1:
+                shapes[i] = [1, 1 / mini]
 
-        if isinstance(self.data, list):
-            self.data_files = data[: round(len(labels) * self.fraction)]
-            self.label_files = labels[: round(len(labels) * self.fraction)]
-            self.data_npy_files = [Path(f).with_suffix(".npy") for f in self.data_files]
-            self.label_npy_files = [Path(f).with_suffix(".npy") for f in self.label_files]
-            self.length = len(self.label_files)
+        self.batch_shapes = np.ceil(np.array(shapes) * self.imgsz / self.stride + self.pad).astype(int) * self.stride
+        self.batch = bi  # batch index of image
 
-            if self.cache == "ram" and self.check_cache_ram():
-                self.cache_data()
-            elif self.cache == "disk" and self.check_cache_disk():
-                self.cache_data()
+    def load(self, i: int) -> tuple[Union[np.ndarray, None], Union[np.ndarray, None]]:
+        """Loads 1 img and label from dataset index 'i', returns (img, label)."""
+        dt, dtf, dtfn = self.data[i], self.data_files[i], self.data_npy_files[i]
+        lb, lbf, lbfn = self.labels[i], self.label_files[i], self.label_npy_files[i]
 
-    def load_image(self, i, rect_mode=True):
-        """Loads 1 image from dataset index 'i', returns (im, resized hw)."""
-        im, f, fn = self.ims[i], self.im_files[i], self.npy_files[i]
-        if im is None:  # not cached in RAM
-            if fn.exists():  # load npy
-                try:
-                    im = np.load(fn)
-                except Exception as e:
-                    LOGGER.warning(f"{self.prefix}WARNING ⚠️ Removing corrupt *.npy image file {fn} due to: {e}")
-                    Path(fn).unlink(missing_ok=True)
-                    im = cv2.imread(f)  # BGR
-            else:  # read image
-                im = cv2.imread(f)  # BGR
-            if im is None:
-                raise FileNotFoundError(f"Image Not Found {f}")
+        # load data
+        if dt is not None:  # cached in RAM
+            data = dt
+        elif dtfn.exists():  # cached in Disk
+            try:
+                data = np.load(dtfn)
+            except Exception as e:
+                LOGGER.warning(f"Removing corrupt *.npy image file {dtfn} and {lbfn} due to: {e}")
+                Path(dtfn).unlink(missing_ok=True)
+                data = self.fread(dtf)
+        else:
+            data = self.fread(dtf)
 
-            h0, w0 = im.shape[:2]  # orig hw
-            if rect_mode:  # resize long side to imgsz while maintaining aspect ratio
-                r = self.imgsz / max(h0, w0)  # ratio
-                if r != 1:  # if sizes are not equal
-                    w, h = (min(math.ceil(w0 * r), self.imgsz), min(math.ceil(h0 * r), self.imgsz))
-                    im = cv2.resize(im, (w, h), interpolation=cv2.INTER_LINEAR)
-            elif not (h0 == w0 == self.imgsz):  # resize by stretching image to square imgsz
-                im = cv2.resize(im, (self.imgsz, self.imgsz), interpolation=cv2.INTER_LINEAR)
+        # load label
+        if lb is not None:  # cached in RAM
+            label = lb
+        elif lbfn.exists():  # cached in Disk
+            try:
+                label = np.load(lbfn)
+            except Exception as e:
+                LOGGER.warning(f"Removing corrupt *.npy image file {dtfn} and {lbfn} due to: {e}")
+                Path(lbfn).unlink(missing_ok=True)
+                label = self.lread(lbf)
+        else:
+            label = self.lread(lbf)
 
-            # Add to buffer if training with augmentations
-            if self.augment:
-                self.ims[i], self.im_hw0[i], self.im_hw[i] = im, (h0, w0), im.shape[:2]  # im, hw_original, hw_resized
-                self.buffer.append(i)
-                if 1 < len(self.buffer) >= self.max_buffer_length:  # prevent empty buffer
-                    j = self.buffer.pop(0)
-                    if self.cache != "ram":
-                        self.ims[j], self.im_hw0[j], self.im_hw[j] = None, None, None
+        h0, w0 = im.shape[:2]  # orig hw
+        if rect_mode:  # resize long side to imgsz while maintaining aspect ratio
+            r = self.imgsz / max(h0, w0)  # ratio
+            if r != 1:  # if sizes are not equal
+                w, h = (min(math.ceil(w0 * r), self.imgsz), min(math.ceil(h0 * r), self.imgsz))
+                im = cv2.resize(im, (w, h), interpolation=cv2.INTER_LINEAR)
+        elif not (h0 == w0 == self.imgsz):  # resize by stretching image to square imgsz
+            im = cv2.resize(im, (self.imgsz, self.imgsz), interpolation=cv2.INTER_LINEAR)
 
-            return im, (h0, w0), im.shape[:2]
+        # Add to buffer if training with augmentations
+        if self.augment:
+            self.data[i] = data
+            self.labels[i] = label  # load to buffer for faster augment
+            self.buffer.append(i)
+            if len(self.buffer) >= self.max_buffer_length:  # prevent empty buffer
+                j = self.buffer.pop(0)
+                if self.cache != "ram":
+                    self.data[j], self.labels[j] = None, None
 
-        return self.ims[i], self.im_hw0[i], self.im_hw[i]
+        return data, label
 
-    def cache_data(self):
-        """Cache data to memory or disk."""
-        pass
+    def update_labels(self, include_class: Optional[list]):
+        """Update labels to include only these classes (optional)."""
+        include_class_array = np.array(include_class).reshape(1, -1)
+        for i in range(len(self.labels)):
+            if include_class is not None:
+                cls = self.labels[i]["cls"]
+                bboxes = self.labels[i]["bboxes"]
+                segments = self.labels[i]["segments"]
+                keypoints = self.labels[i]["keypoints"]
+                j = (cls == include_class_array).any(1)
+                self.labels[i]["cls"] = cls[j]
+                self.labels[i]["bboxes"] = bboxes[j]
+                if segments:
+                    self.labels[i]["segments"] = [segments[si] for si, idx in enumerate(j) if idx]
+                if keypoints is not None:
+                    self.labels[i]["keypoints"] = keypoints[j]
+            if self.single_cls:
+                self.labels[i]["cls"][:, 0] = 0
 
-    def cache_data_to_disk(self, i):
-        """Saves data as a *.npy file for faster loading."""
-        pass
+    def check_data_cache_ram(self, safety_margin=0.5):
+        return super().check_data_cache_ram(safety_margin)
 
-    def check_cache_disk(self, safety_margin=0.5):
-        """Check data caching requirements vs available disk space."""
-        pass
-
-    def check_cache_ram(self, safety_margin=0.5):
-        """Check image caching requirements vs available memory."""
-        pass
-
-    def __getitem__(self, index):
-        """Returns transformed label information for given index."""
-        return self.transforms(self.get_image_and_label(index))
-
-    def get_data_and_label(self, index):
-        """Get and return label information from the dataset."""
-        label = deepcopy(self.labels[index])  # requires deepcopy() https://github.com/ultralytics/ultralytics/pull/1948
-        label.pop("shape", None)  # shape is for rect, remove it
-        label["img"], label["ori_shape"], label["resized_shape"] = self.load_image(index)
-        label["ratio_pad"] = (
-            label["resized_shape"][0] / label["ori_shape"][0],
-            label["resized_shape"][1] / label["ori_shape"][1],
-        )  # for evaluation
-        return self.update_labels_info(label)
-
-    def __len__(self):
-        """Returns the length of the labels list for the dataset."""
-        return len(self.labels)
-
-    def update_labels_info(self, label):
-        """Custom your label format here."""
-        return label
+    def check_data_cache_disk(self, safety_margin=0.5):
+        return super().check_data_cache_disk(safety_margin)
 
     def build_transforms(self, hyp=None):
         """Builds and appends transforms to the list."""
@@ -192,27 +208,6 @@ class YoloDataset(DatasetBase):
         hyp.copy_paste = 0.0  # keep the same behavior as previous v8 close-mosaic
         hyp.mixup = 0.0  # keep the same behavior as previous v8 close-mosaic
         self.transforms = self.build_transforms(hyp)
-
-    def get_labels(self):
-        """
-        Users can customize their own format here.
-
-        Note:
-            Ensure output is a dictionary with the following keys:
-            ```python
-            dict(
-                im_file=im_file,
-                shape=shape,  # format: (height, width)
-                cls=cls,
-                bboxes=bboxes,  # xywh
-                segments=segments,  # xy
-                keypoints=keypoints,  # xy
-                normalized=True,  # or False
-                bbox_format="xyxy",  # or xywh, ltwh
-            )
-            ```
-        """
-        raise NotImplementedError
 
 
 class MultimodalDataset(DatasetBase):
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,4 +1,10 @@
-import random
+import numpy as np
+from pympler import asizeof
 
-a = None
-print(a.nbytes)
+a = {"cls": 1, "bboxes": np.array([0.1, 0.2, 0.3, 0.4])}
+b = {"cls": 2, "bboxes": np.array([0.8, 0.6, 0.5, 0.4])}
+c = {"cls": 3, "bboxes": np.array([0.4, 0.6, 0.3, 0.1])}
+d = [a, b, c]
+print(asizeof.asizeof(d))
+e = {"cls": np.array([1, 2, 3]), "bboxes": np.array([[0.1, 0.2, 0.3, 0.4], [0.8, 0.6, 0.5, 0.4], [0.4, 0.6, 0.3, 0.1]])}
+print(asizeof.asizeof(e))
