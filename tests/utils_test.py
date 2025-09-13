b6e5e69e45f4db7d77e332691f78fad299747309
WSYANGSX <1062033103@qq.com>
2025-09-13 08:48:16 +0800

    modified

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,7 +1,1 @@
-import numpy as np
-from pympler import asizeof
-from machine_learning.utils.detection import class_maps
-
-b = ["cat", "pig", "dog"]
-c = class_maps(b)
-print(c["0"])
+from torchvision.transforms import Compose, ToTensor
