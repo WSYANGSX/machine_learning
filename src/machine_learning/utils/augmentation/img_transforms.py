50900db74ad72c979f371f8dcfa991dac7e66079
WSYANGSX <1062033103@qq.com>
2025-12-04 09:01:29 +0800

    modified

--- src/machine_learning/utils/augmentation/img_transforms.py
+++ src/machine_learning/utils/augmentation/img_transforms.py
@@ -2280,6 +2280,7 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
         [
             pre_transform,
             MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
+            CutMix(dataset, pre_transform=pre_transform, p=hyp.cutmix),
             Albumentations(p=1.0),
             RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
             RandomFlip(direction="vertical", p=hyp.flipud),
