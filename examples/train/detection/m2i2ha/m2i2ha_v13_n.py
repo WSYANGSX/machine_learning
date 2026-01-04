66ca4f2ef9bcff35dd4e2962e9d50e42cf0e3cc2
WSYANGSX <1062033103@qq.com>
2026-01-04 09:11:30 +0800

    renamed

---
+++ examples/train/detection/m2i2ha/m2i2ha_v13_n.py
@@ -0,0 +1,31 @@
+import time
+from machine_learning.networks.yolo.m2i2ha import M2I2HANet_v13
+from machine_learning.algorithms.detection import MultimodalDetection
+from machine_learning.trainer import Trainer, TrainerCfg
+
+
+def main():
+    # Step 0: build network (optional)
+    net = M2I2HANet_v13(640, nc=5, net_scale="n")
+
+    # Step 1: Parse the data
+    m2i2ha = MultimodalDetection("m2i2ha.yaml", net=net, amp=True)
+
+    # Step 2: Configure the trainer
+    trainer_cfg = TrainerCfg(
+        log_dir="/home/yangxf/WorkSpace/machine_learning/logs/m2i2ha/",
+        ckpt_dir="/home/yangxf/WorkSpace/machine_learning/checkpoints/m2i2ha/",
+        epochs=150,
+        log_interval=10,
+        save_interval=10,
+        save_best=True,
+        seed=int(time.time()),
+    )
+    trainer = Trainer(trainer_cfg, m2i2ha, "drone_vehicle.yaml")
+
+    # Step 3: Train the model
+    trainer.train()
+
+
+if __name__ == "__main__":
+    main()
