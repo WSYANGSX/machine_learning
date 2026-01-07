a5009c278be255c7128193d09d0d8981997e3697
WSYANGSX <1062033103@qq.com>
2026-01-07 10:08:20 +0800

    modified

--- examples/train/detection/run.sh
+++ examples/train/detection/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# 顺序运行两个程序（不是同时运行）
-echo "开始运行程序1..."
-/home/yangxf/anaconda3/envs/ai/bin/python yolo_v8_ir.py
+# # 顺序运行两个程序（不是同时运行）
+# echo "开始运行程序1..."
+# /home/yangxf/anaconda3/envs/ai/bin/python yolo_v8_ir.py
 
-echo "开始运行程序2..."
-/home/yangxf/anaconda3/envs/ai/bin/python yolo_v8_rgb.py
+# echo "开始运行程序2..."
+# /home/yangxf/anaconda3/envs/ai/bin/python yolo_v8_rgb.py
 
 echo "开始运行程序3..."
 /home/yangxf/anaconda3/envs/ai/bin/python yolo_v13_ir.py
