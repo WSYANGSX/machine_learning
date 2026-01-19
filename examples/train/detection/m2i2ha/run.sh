893495aa4c464bab98c91158b4346926e0697fab
WSYANGSX <1062033103@qq.com>
2026-01-19 18:31:24 +0800

    modified

--- examples/train/detection/m2i2ha/run.sh
+++ examples/train/detection/m2i2ha/run.sh
@@ -1,14 +1,8 @@
 # 顺序运行两个程序（不是同时运行）
 echo "开始运行程序1..."
-/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_v8_n.py
+/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_baselines.py --hyperace
 
 echo "开始运行程序2..."
-/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_v8_s.py
+/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_baselines.py
 
-echo "开始运行程序3..."
-/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_v13_n.py
-
-echo "开始运行程序4..."
-/home/yangxf/anaconda3/envs/ai/bin/python m2i2ha_v13_s.py
-
-echo "程序都完成了"
+echo "两个程序都完成了"
