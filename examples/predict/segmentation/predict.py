91212013c65d63e9ddec210412ec394d52b7f859
WSYANGSX <1062033103@qq.com>
2026-04-22 15:50:56 +0800

    modified:   examples/eval/segmentation/eval.py modified:   examples/predict/segmentation/predict.py modified:   tests/1.py

--- examples/predict/segmentation/predict.py
+++ examples/predict/segmentation/predict.py
@@ -4,11 +4,11 @@ from machine_learning.evaluator import Predictor
 def main():
     # Step 1: Build the predictor from ckpt
     predictor = Predictor(
-        "/home/yangxf/WorkSpace/machine_learning/runs/unet/unet__car_2026-04-22_11-29/ckpt/best_model.pth",
+        "/home/yangxf/WorkSpace/machine_learning/runs/unet/unet_sbd_2026-04-15_16-43/ckpt/best_model.pth",
     )
 
     # Step 3: Predict
-    predictor.algorithm.predict(stream="/home/yangxf/WorkSpace/datasets/..datasets/car/imgs/test/3f3e362dea23_01.jpg")
+    predictor.algorithm.predict(stream="/home/yangxf/WorkSpace/datasets/..datasets/car/imgs/test/3f202616a2b9_11.jpg")
 
 
 if __name__ == "__main__":
