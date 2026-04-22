51347f821f945cb265badbdfc0f916b4bd1bfb6b
WSYANGSX <1062033103@qq.com>
2026-04-22 15:50:56 +0800

    modified:   examples/eval/segmentation/eval.py modified:   examples/predict/segmentation/predict.py modified:   tests/1.py

--- tests/1.py
+++ tests/1.py
@@ -1,1 +1,15 @@
+import cv2
+import numpy as np
+from PIL import Image
 
+
+img = Image.open("/home/yangxf/WorkSpace/datasets/..datasets/car/masks/train/0cdf5b5d0ce1_01.jpg")
+print(img.mode)
+img = np.array(img)
+print(img.shape)
+print(img.max(), img.min())
+
+
+img = cv2.imread("/home/yangxf/WorkSpace/datasets/..datasets/car/masks/train/0cdf5b5d0ce1_01.jpg", cv2.IMREAD_UNCHANGED)
+print(img.shape)
+print(img.max(), img.min())
